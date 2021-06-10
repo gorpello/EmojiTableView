ef81ce3bb1185ebbfdd2e58210732324f9f1c66f
Gianluca Orpello <g.orpello@gmail.com>
2021-06-10 19:27:39 +0200

    Initial Commit

---
+++ TableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by Gianluca Orpello on 10/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
